0c1ab118b63cbb911af476a7173cca5c579f3a4a
Julian <53325429+LittieRain@users.noreply.github.com>
2022-07-08 13:21:11 +1000

    Set of tasks and challenges
    Task 1 with 10 scenarios and challenge 1 and 2

---
+++ Task2/Challenge1/ConsoleApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace FIB
+{
+    class Program
+    {
+        private static Dictionary<int, BigInteger> calculationCache;
+        private static BigInteger Fibonacci(int num)
+        {
+            BigInteger result = 0;
+
+            // Check if the number is in the cache, if so return the value
+            // ! Cache is used to avoid recalculating the same numbers
+            // ! Massive performance could be boost if fibonacci sequence is large
+            if (calculationCache.ContainsKey(num))
+            {
+                return calculationCache[num];
+            }
+
+            // Otherwise calculate the value and add it to the cache
+            if (num == 1 || num == 2)
+            {
+                result = 1;
+            }
+            else if (num > 0)
+            {
+                result = Fibonacci(num - 1) + Fibonacci(num - 2);
+                calculationCache.Add(num, result);
+            }
+
+            return result;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            BigInteger number = 0;
+            calculationCache = new Dictionary<int, BigInteger>();
+            // 1. Generate Fibonacci sequence
+            List<BigInteger> fibSequence = new List<BigInteger>();
+            for (int i = 1; i <= 1000; i++)
+            {
+                fibSequence.Add(Fibonacci(i));
+            }
+            //print the sequence
+            //foreach (BigInteger i in fibSequence)
+            //   Console.WriteLine(i);
+            Console.Write("Please enter a number >> ");
+            number = BigInteger.Parse(Console.ReadLine());
+            
+            if (fibSequence.Contains(number))
+            {
+                Console.WriteLine("This number is a FIB number, and it's at {0} postion", fibSequence.IndexOf(number));
+            }
+            else
+            {
+                Console.WriteLine("It's not a FIB number");
+            }
+        }
+    }
+}
---
+++ Task2/Challenge2/Maxchar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Maxchar
+{
+    class Program
+    {
+
+        const int ASCII = 256;
+        private static char getMaxChar(String str)
+        {
+            int[] count = new int[ASCII];
+            int max = -1;
+            char maxchar = ' ';
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                
+                count[str[i]]++;
+            }
+
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (max < count[str[j]])
+                {
+                    max = count[str[j]];
+                    maxchar = str[j];
+                }
+            }
+
+            return maxchar;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string x;
+            string j;
+            Console.Write("Please enter strings >> ");
+            x = Console.ReadLine();
+            j = x.ToLower();
+            Console.WriteLine("The max occurring string is {0}", getMaxChar(j));
+        }
+    }
+}
